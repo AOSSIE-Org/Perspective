3aef07d90817ce544885f0af84caf6677ef72d15
ParagGhatage <phghatage1@gmail.com>
2025-06-08 18:44:13 +0530

    chenged port-number

--- new-backend/main.py
+++ new-backend/main.py
@@ -14,5 +14,5 @@ app.include_router(article_router,prefix="/api",tags=["Articles"])
 if __name__ == "__main__":
     import uvicorn
     print("server is running on http://localhost:8000/api")
-    uvicorn.run("main:app",host="0.0.0.0",port=8001,reload=True)
+    uvicorn.run("main:app",host="0.0.0.0",port=8000,reload=True)
    
