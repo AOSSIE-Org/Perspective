2780cae601a01fcfd23e0fe5ceff14550cf2219d
Parag Ghatage <parag.dev2025@gmail.com>
2025-07-02 12:15:24 +0530

    resolved some bugs and applied changes to prevent nexpected errors

--- new-backend/app/modules/langgraph_nodes/fact_check.py
+++ new-backend/app/modules/langgraph_nodes/fact_check.py
@@ -1,4 +1,5 @@
-from app.modules.pipeline import run_fact_check_pipeline
+
+from app.utils.fact_check_utils import run_fact_check_pipeline
 
 
 def run_fact_check(state):
@@ -8,7 +9,16 @@ def run_fact_check(state):
         if not text:
             raise ValueError("Missing or empty 'cleaned_text' in state")
 
-        verifications = run_fact_check_pipeline(state)
+        verifications, error_message = run_fact_check_pipeline(state)
+
+        if error_message:
+            print(f"some error occured in fact_checking:{error_message}")
+            return {
+                "status": "error",
+                "error_from": "fact_checking",
+                "message": f"{error_message}",
+            }
+
     except Exception as e:
         print(f"some error occured in fact_checking:{e}")
         return {
